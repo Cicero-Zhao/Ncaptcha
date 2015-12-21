05fd47e45a45b532da802ea3f10213ba50a2ac02
cicero <zhaorbox@gmail.com>
2015-12-21 15:23:28 +0800

    init
    init

---
+++ ncaptcha/__init__.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+
+__author__ = 'barycenter'
+
+from PIL import Image, ImageDraw
+import StringIO
+
+class Ncaptch(object):
+    def __init__(self, random_str='', image_suffix="png"):
+        self.width = 186
+        self.heigt = 65
+        pass
+
+    def draw(self):
+
+        pass
+
